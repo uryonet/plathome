f237d74436ac5519d919987abf3766ea765f5231
uryonet <uryonet@outlook.com>
2021-03-30 22:24:22 +0900

    #1 ナビゲーションメニューにルーティング設定を追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,26 @@
 import React from 'react'
-import logo from './logo.svg'
-
-import { AuthenticatedTemplate, UnauthenticatedTemplate, useMsal } from '@azure/msal-react'
+import { Route, Switch } from 'react-router-dom'
+import PrivateRoute from './lib/PrivateRoute'
+import Home from './Home'
+import Tasks from './Tasks'
+import Notes from './Notes'
 
 import './App.css'
-import { Button } from '@material-ui/core'
 
 const App: React.FC = () => {
-  const { instance } = useMsal()
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-        <AuthenticatedTemplate>
-          <p>ログインしています。</p>
-          <Button variant="contained" color="secondary" onClick={() => instance.logoutRedirect()}>
-            ログアウト
-          </Button>
-        </AuthenticatedTemplate>
-        <UnauthenticatedTemplate>
-          <p>ログインしていません。</p>
-          <Button variant="contained" color="primary" onClick={() => instance.loginRedirect()}>
-            ログイン
-          </Button>
-        </UnauthenticatedTemplate>
-      </header>
+      <Switch>
+        <PrivateRoute path="/tasks">
+          <Tasks />
+        </PrivateRoute>
+        <PrivateRoute path="/notes">
+          <Notes />
+        </PrivateRoute>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
     </div>
   )
 }
