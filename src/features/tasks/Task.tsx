df28bd864eb7498f089f0f051e422d88cd70cc83
uryonet <uryonet@outlook.com>
2021-04-16 17:40:44 +0900

    #12 タスク詳細ページに更新ボタンと削除ボタンを設置

--- src/features/tasks/Task.tsx
+++ src/features/tasks/Task.tsx
@@ -4,42 +4,85 @@ import { useParams } from 'react-router-dom'
 import { fetchToggleStatus, selectTasks } from './tasksSlice'
 
 import { TodoTask } from 'microsoft-graph'
-import { Box, Checkbox, TextField } from '@material-ui/core'
+import { Box, Button, Checkbox, createStyles, makeStyles, TextField } from '@material-ui/core'
+import { Delete, Send } from '@material-ui/icons'
 
 interface ParamTypes {
   taskId: string
 }
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    button: {
+      marginTop: theme.spacing(1),
+      marginBottom: theme.spacing(1)
+    }
+  })
+)
 
 const Task: React.FC = () => {
   const { taskId } = useParams<ParamTypes>()
   const { taskListId, tasks } = useSelector(selectTasks)
   const [currentTask, setCurrentTask] = useState<TodoTask>({})
   const dispatch = useDispatch()
+  const styles = useStyles()
 
   useEffect(() => {
+    console.log('タスク詳細ページ初期化処理')
     const selectedTask = tasks.find((task) => task.id === taskId)
     if (selectedTask) {
       setCurrentTask(selectedTask)
     }
-  }, [])
+  }, [taskId, tasks])
 
   const handleClickToggle = () => {
     dispatch(fetchToggleStatus(taskListId, currentTask))
-
     const task = { ...currentTask }
     task.status = currentTask.status === 'completed' ? 'notStarted' : 'completed'
     setCurrentTask(task)
   }
 
+  const handleSendBtn = () => {
+    console.log('send button')
+  }
+
+  const handleDelBtn = () => {
+    console.log('del button')
+  }
+
   return (
-    <Box className="task-detail" display="flex">
-      <Checkbox edge="start" checked={currentTask.status === 'completed'} onChange={handleClickToggle} />
-      <TextField
-        fullWidth
-        multiline
-        value={currentTask.title}
-        inputProps={{ style: { fontSize: 20, lineHeight: 1.3 } }}
-      />
+    <Box className="task-detail">
+      <Box className="task-detail-info" display="flex">
+        <Checkbox edge="start" checked={currentTask.status === 'completed'} onChange={handleClickToggle} />
+        <TextField
+          fullWidth
+          multiline
+          value={currentTask.title}
+          inputProps={{ style: { fontSize: 20, lineHeight: 1.3 } }}
+        />
+      </Box>
+      <Box>
+        <Button
+          className={styles.button}
+          fullWidth
+          variant="outlined"
+          color="primary"
+          startIcon={<Send />}
+          onClick={handleSendBtn}
+        >
+          更新
+        </Button>
+        <Button
+          className={styles.button}
+          fullWidth
+          variant="outlined"
+          color="secondary"
+          startIcon={<Delete />}
+          onClick={handleDelBtn}
+        >
+          削除
+        </Button>
+      </Box>
     </Box>
   )
 }
